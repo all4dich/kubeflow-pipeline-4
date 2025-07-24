806f6e46117ee7b8e1c2fdc350796fa7e7f24578
Sunjoo Park <all4dich@gmail.com>
2025-07-24 09:45:57 +0900

    feat(pipeline): add session-based authentication for Kubeflow

--- pipeline_remote_control.py
+++ pipeline_remote_control.py
@@ -1,10 +1,17 @@
 import kfp
 from kfp import dsl
 from kfp.dsl import component
+import requests
 
-HOST = "https://kubeflow.sunjoo.org"
 # Replace with your actual Kubernetes service account token
 KUBERNETES_SERVICE_ACCOUNT_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6Inkzc2VfLTlxVDRuczNQTXRERjBaY0pWUWgyazRjczd1WDBtREhreUtjWW8ifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNzUzMjgzMDc4LCJpYXQiOjE3NTMyNzk0NzgsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwianRpIjoiNTZkOGI1MmMtNTA2Mi00MDFkLWE2NTgtZGQ4MDc4NzQ4OGU3Iiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJrdWJlZmxvdyIsInNlcnZpY2VhY2NvdW50Ijp7Im5hbWUiOiJkZWZhdWx0IiwidWlkIjoiOTcyZGVhNTUtOTZlNC00MDRlLTkwZTUtOWVjNGY5MzEwZGExIn19LCJuYmYiOjE3NTMyNzk0NzgsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDprdWJlZmxvdzpkZWZhdWx0In0.rVHrc2Jm2SMvksp_6xaanpiwpjbhg0heKqhS3NJOJfFPDKMkSktsmKAL8cfzv3riY4AVpFta_PtQR-JamsPj67nnVE4Q8fMesdQyT3wRpc3JQT5-X9mz7X-BoP3HNMY1KuDaYCd8YpPtI-NknKOLJa7s8pd9lbNgWwNBgnss-DD3oAJILtRUd4Q0A1yiU1B9i7rerTKM3D3983VMvv6vo7o8h2XlzcBjLituKpZhRuSEe0z8poMXS8IQyF4T4djhTk3_cWnn_M_GgUDirxtFpJeuco1E-JaH3fUK_1WG1aZmQnpWbU5cSxxZVquznMHTZAxqMsYxkIFY1-UDdkyz4g"
+HOST  = "https://kubeflow.keti-dp.re.kr/"
+USERNAME = "yjwon@keti.re.kr"
+PASSWORD = "won1234!"
+
+#HOST  = "https://kubeflow.sunjoo.org"
+#USERNAME = "sunjoo.park"
+#PASSWORD = "Postech2001!"
 
 def check_by_existing_token():
     print("Attempting to authenticate with Kubernetes service account token and list pipelines...")
@@ -20,5 +27,34 @@ def check_by_existing_token():
         print(f"Failed to list pipelines: {e}")
         print("Please ensure your KUBERNETES_SERVICE_ACCOUNT_TOKEN is valid and has the necessary permissions.")
 
+def get_kubeflow_session_cookie():
+    session = requests.Session()
+    response = session.get(HOST)
+
+    headers = {
+        #"Content-Type": "application/json",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+    user_data = {"login": USERNAME, "password": PASSWORD}
+    response = session.post(response.url, data=user_data, verify=False)
+    if response.status_code == 200:
+        print("🔐 Successfully authenticated to Kubeflow.")
+        return session.cookies.get("authservice_session")
+    else:
+        print("❌ Authentication failed. Check your credentials.")
+        print(response.status_code)
+        exit(1)
+
+def check_by_session_call(host, username, password, namespace):
+    print("Attempting to authenticate with Kubernetes session and list pipelines...")
+    client = kfp.Client(
+        host=f"{host}/pipeline",
+        namespace=namespace,
+        cookies=f"authservice_session={get_kubeflow_session_cookie()}"
+    )
+    r = client.list_pipelines()
+    print(r)
+
 if __name__ == '__main__':
-    check_by_existing_token()
+    #check_by_existing_token()
+    check_by_session_call(host=HOST, username=USERNAME, password=PASSWORD, namespace="space-yjwon")
