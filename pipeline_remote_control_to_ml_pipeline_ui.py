493e87f5d483dbd2bdd5bd3ddd8135564c8e92f8
Sunjoo Park <all4dich@gmail.com>
2025-07-28 12:54:25 +0900

    feat(pipeline): refactor authentication to use environment variables for configuration

--- pipeline_remote_control_to_ml_pipeline_ui.py
+++ pipeline_remote_control_to_ml_pipeline_ui.py
@@ -3,6 +3,7 @@ from kfp import dsl
 from kfp.dsl import component
 import requests
 from bs4 import BeautifulSoup
+import os
 
 # Replace with your actual Kubernetes service account token
 KUBERNETES_SERVICE_ACCOUNT_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6Inkzc2VfLTlxVDRuczNQTXRERjBaY0pWUWgyazRjczd1WDBtREhreUtjWW8ifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNzUzMjgzMDc4LCJpYXQiOjE3NTMyNzk0NzgsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwianRpIjoiNTZkOGI1MmMtNTA2Mi00MDFkLWE2NTgtZGQ4MDc4NzQ4OGU3Iiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJrdWJlZmxvdyIsInNlcnZpY2VhY2NvdW50Ijp7Im5hbWUiOiJkZWZhdWx0IiwidWlkIjoiOTcyZGVhNTUtOTZlNC00MDRlLTkwZTUtOWVjNGY5MzEwZGExIn19LCJuYmYiOjE3NTMyNzk0NzgsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDprdWJlZmxvdzpkZWZhdWx0In0.rVHrc2Jm2SMvksp_6xaanpiwpjbhg0heKqhS3NJOJfFPDKMkSktsmKAL8cfzv3riY4AVpFta_PtQR-JamsPj67nnVE4Q8fMesdQyT3wRpc3JQT5-X9mz7X-BoP3HNMY1KuDaYCd8YpPtI-NknKOLJa7s8pd9lbNgWwNBgnss-DD3oAJILtRUd4Q0A1yiU1B9i7rerTKM3D3983VMvv6vo7o8h2XlzcBjLituKpZhRuSEe0z8poMXS8IQyF4T4djhTk3_cWnn_M_GgUDirxtFpJeuco1E-JaH3fUK_1WG1aZmQnpWbU5cSxxZVquznMHTZAxqMsYxkIFY1-UDdkyz4g"
@@ -138,12 +139,11 @@ def check_request(host: str, username: str, password: str, auth_suffix: str = "/
 
 if __name__ == '__main__':
     #HOST = "http://34.82.249.126:8090"
-    HOST = "https://kubeflow.sunjoo.org"
-    USERNAME = "user@example.com"
-    PASSWORD = "12341234"
-    #USERNAME = "sunjoo.park"
-    #PASSWORD = "Postech2001!"
-    cookie_value = check_request(HOST, USERNAME, PASSWORD, auth_type="local")
+    HOST = os.environ.get("KUBEFLOW_HOST", "https://kubeflow.sunjoo.org")
+    USERNAME = os.environ.get("KUBEFLOW_USERNAME", "user@example.com")
+    PASSWORD = os.environ.get("KUBEFLOW_PASSWORD", "12341234")
+    AUTH_SOURCE = os.environ.get("KUBEFLOW_AUTH_SOURCE", "local")
+    cookie_value = check_request(HOST, USERNAME, PASSWORD, auth_type=AUTH_SOURCE)
     client = kfp.Client(
         host=HOST + "/pipeline",
         cookies=cookie_value
