3752c5caf74f699a0314afc600122dbdc2fa588b
Sunjoo Park <all4dich@gmail.com>
2025-07-28 12:04:28 +0900

    feat(pipeline): Enable to get kubeflow authentication for 1.8.1 and 1.9.1

--- pipeline_remote_control_to_ml_pipeline_ui.py
+++ pipeline_remote_control_to_ml_pipeline_ui.py
@@ -2,6 +2,7 @@ import kfp
 from kfp import dsl
 from kfp.dsl import component
 import requests
+from bs4 import BeautifulSoup
 
 # Replace with your actual Kubernetes service account token
 KUBERNETES_SERVICE_ACCOUNT_TOKEN = "eyJhbGciOiJSUzI1NiIsImtpZCI6Inkzc2VfLTlxVDRuczNQTXRERjBaY0pWUWgyazRjczd1WDBtREhreUtjWW8ifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNzUzMjgzMDc4LCJpYXQiOjE3NTMyNzk0NzgsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwianRpIjoiNTZkOGI1MmMtNTA2Mi00MDFkLWE2NTgtZGQ4MDc4NzQ4OGU3Iiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJrdWJlZmxvdyIsInNlcnZpY2VhY2NvdW50Ijp7Im5hbWUiOiJkZWZhdWx0IiwidWlkIjoiOTcyZGVhNTUtOTZlNC00MDRlLTkwZTUtOWVjNGY5MzEwZGExIn19LCJuYmYiOjE3NTMyNzk0NzgsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDprdWJlZmxvdzpkZWZhdWx0In0.rVHrc2Jm2SMvksp_6xaanpiwpjbhg0heKqhS3NJOJfFPDKMkSktsmKAL8cfzv3riY4AVpFta_PtQR-JamsPj67nnVE4Q8fMesdQyT3wRpc3JQT5-X9mz7X-BoP3HNMY1KuDaYCd8YpPtI-NknKOLJa7s8pd9lbNgWwNBgnss-DD3oAJILtRUd4Q0A1yiU1B9i7rerTKM3D3983VMvv6vo7o8h2XlzcBjLituKpZhRuSEe0z8poMXS8IQyF4T4djhTk3_cWnn_M_GgUDirxtFpJeuco1E-JaH3fUK_1WG1aZmQnpWbU5cSxxZVquznMHTZAxqMsYxkIFY1-UDdkyz4g"
@@ -96,38 +97,56 @@ def get_kubeflow_session_cookie(target_host, target_username, target_password):
         exit(1)
 
 
+def extract_hrefs_from_string(html_string):
+    """Extract href values from HTML string"""
+    soup = BeautifulSoup(html_string, 'html.parser')
+    links = soup.find_all('a', href=True)
+    return [link['href'] for link in links]
+
+
 def check_request(host: str, username: str, password: str, auth_suffix: str = "/oauth2/start", auth_type="ldap"):
     session = requests.Session()
     auth_url = f"{host}{auth_suffix}"
     session_response = session.get(auth_url)
-    cookie_value_1 = session.cookies.get('oidc_state_csrf')
-    auth_request_url = f"{host}/dex/auth/{auth_type}?client_id=kubeflow-oidc-authservice&redirect_uri=%2Fauthservice%2Foidc%2Fcallback&response_type=code&scope=openid+profile+email+groups&state={cookie_value_1}"
-    auth_response = session.get(auth_request_url, cookies={'oidc_state_csrf': cookie_value_1}, verify=False)
-    login_request_url  = auth_response.url
-    login_response = session.post(login_request_url, data={"login": username, "password": password}, verify=False, cookies={"oidc_state_csrf": cookie_value_1})
+    # Since 1.9.1 cookie's name is `oauth2_proxy_kubeflow_csrf`
+    # cookie_name = "oidc_state_csrf" # For Kubeflow Manifest 1.8.1
+    # cookie_name = "oauth2_proxy_kubeflow_csrf"
+    cookie_name = session.cookies.keys()[0]
+    cookie_value_1 = session.cookies.get(cookie_name)
+    href_values = extract_hrefs_from_string(session_response.text)
+    auth_request_url = ""
+    for href_url in href_values:
+        if href_url.split("/")[3].split("?")[0] == auth_type:
+            auth_request_url = host + href_url
+            break
+    auth_response = session.get(auth_request_url, cookies={cookie_name: cookie_value_1}, verify=False)
+    login_request_url = auth_response.url
+    login_response = session.post(login_request_url, data={"login": username, "password": password}, verify=False,
+                                  cookies={cookie_name: cookie_value_1})
     login_response.raise_for_status()
     print(session.cookies)
-    cookie_name = "oauth2_proxy_kubeflow"
-    if auth_suffix == "":
-        cookie_name = "authservice_session"
-    cookie_value = session.cookies.get(cookie_name)
-    return f"{cookie_name}={cookie_value}"
+    cookie_return = None
+    for a_cookie in session.cookies:
+        # login_cookie_name = "oauth2_proxy_kubeflow" # For Kubeflow 1.9.1 and later
+        # login_cookie_name = "authservice_session" # For Kubeflow 1.8.1
+        login_cookie_name = a_cookie.name
+        login_cookie_value = a_cookie.value
+        cookie_return = f"{login_cookie_name}={login_cookie_value}"
+        break
+    return cookie_return
+
 
 if __name__ == '__main__':
-    #HOST = "http://35.185.198.208:8090"
+    #HOST = "http://34.82.249.126:8090"
     HOST = "https://kubeflow.sunjoo.org"
     USERNAME = "user@example.com"
     PASSWORD = "12341234"
     #USERNAME = "sunjoo.park"
     #PASSWORD = "Postech2001!"
-    cookie_value = check_request(HOST, USERNAME, PASSWORD, auth_suffix="", auth_type="local")
-    #cookie_value = check_request(HOST, USERNAME, PASSWORD, "")
-    #client = kfp.Client(host=HOST, existing_token="eyJhbGciOiJSUzI1NiIsImtpZCI6InhnMnItc2dJelFNbWlPcW4zRml2eUwyNjZSV3l0MU9oRXZoQVJudXhhM1EifQ.eyJhdWQiOlsiaHR0cHM6Ly9rdWJlcm5ldGVzLmRlZmF1bHQuc3ZjLmNsdXN0ZXIubG9jYWwiXSwiZXhwIjoxNzUzNTQ2NzU3LCJpYXQiOjE3NTM1NDMxNTcsImlzcyI6Imh0dHBzOi8va3ViZXJuZXRlcy5kZWZhdWx0LnN2Yy5jbHVzdGVyLmxvY2FsIiwia3ViZXJuZXRlcy5pbyI6eyJuYW1lc3BhY2UiOiJrdWJlZmxvdy11c2VyLWV4YW1wbGUtY29tIiwic2VydmljZWFjY291bnQiOnsibmFtZSI6ImRlZmF1bHQiLCJ1aWQiOiJlM2ZhZDA0OS02ZGI3LTQzYjAtOTk2NC03ZTE4MTdmMTg5ZWIifX0sIm5iZiI6MTc1MzU0MzE1Nywic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Omt1YmVmbG93LXVzZXItZXhhbXBsZS1jb206ZGVmYXVsdCJ9.KRbyCxTAPdUW1FEqw1TVnbK0esO_K_s73MTPziVB-EHp6HOjfyttiZFJuZ_2Mw3NlxCays2f6yCEh0ZSLkOD3nm1DIfMhyk4BzWE0dd91mTpO-uqtgipI6PtByw8S6balUkKAyzZsEKF7mkQPeE8a8lgsOw-Krg7ihH-SVsLzR1QE3XGeDlK4ushpetkubpwEOL1yhhEQwBsBnuVrAFiChLXLyV447zjd-dJtoYWUegsfV4jaZX4a6KRR5esRuLUqYOJ75KjOhhhTXudg7cvQOLvl2fHhP5bje58nKInI6ukyWyTdXgVZwd7fRjfleY19Dln70MOesARtkU_xJaZlg")
+    cookie_value = check_request(HOST, USERNAME, PASSWORD, auth_type="local")
     client = kfp.Client(
         host=HOST + "/pipeline",
         cookies=cookie_value
     )
     r = client.list_pipelines()
-    #r2 = client.get_pipeline("5759ebbd-defc-4cd0-ad34-e50e0ff446cf")
     print(r)
-    #print(r2)
